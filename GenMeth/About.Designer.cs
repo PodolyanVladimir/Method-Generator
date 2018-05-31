aa1b2994159b0411a1ab395ccb58328fb1b05987
vladimir Podolyan <vladimir.podolyan64@gmail.com>
2018-05-31 13:13:02 +0400

    Change picture again :)

--- GenMeth/About.Designer.cs
+++ GenMeth/About.Designer.cs
@@ -46,9 +46,9 @@ namespace GenMeth
 			// pictureBox1
 			// 
 			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-			this.pictureBox1.Location = new System.Drawing.Point(-78, 52);
+			this.pictureBox1.Location = new System.Drawing.Point(-113, 37);
 			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(598, 311);
+			this.pictureBox1.Size = new System.Drawing.Size(664, 339);
 			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
 			this.pictureBox1.TabIndex = 0;
 			this.pictureBox1.TabStop = false;
@@ -56,11 +56,12 @@ namespace GenMeth
 			// 
 			// label1
 			// 
-			this.label1.AutoSize = true;
+			this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
 			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.label1.Location = new System.Drawing.Point(66, 9);
+			this.label1.ForeColor = System.Drawing.Color.DarkOrange;
+			this.label1.Location = new System.Drawing.Point(-2, -2);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(318, 40);
+			this.label1.Size = new System.Drawing.Size(450, 45);
 			this.label1.TabIndex = 1;
 			this.label1.Text = "Генератор методов создания объектов \r\nлокального представления данных";
 			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -68,14 +69,15 @@ namespace GenMeth
 			// 
 			// label2
 			// 
-			this.label2.AutoSize = true;
+			this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
 			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.label2.Location = new System.Drawing.Point(12, 317);
+			this.label2.ForeColor = System.Drawing.Color.DarkOrange;
+			this.label2.Location = new System.Drawing.Point(-2, 332);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(419, 80);
+			this.label2.Size = new System.Drawing.Size(450, 67);
 			this.label2.TabIndex = 2;
 			this.label2.Text = "Дипломный проект студента\r\nГАПОУ АО \"Вельский индустриально-экономический колледж" +
-			"\"\r\nПодоляна Владимира Андреевича\r\nВельск\r\n2018";
+			"\"\r\nПодолян Владимира Андреевича\r\nВельск 2018";
 			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			this.label2.Click += new System.EventHandler(this.Label2Click);
 			// 
@@ -94,7 +96,6 @@ namespace GenMeth
 			this.Click += new System.EventHandler(this.AboutClick);
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			this.ResumeLayout(false);
-			this.PerformLayout();
 		}
 		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.Label label1;
